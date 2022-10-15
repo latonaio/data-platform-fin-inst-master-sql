83dac96a2cb8da91541f7d3dc53a343e219ea93c
Yurino Oga <yurino.o@latona.io>
2022-10-15 11:01:09 +0900

    release

--- data-platform-fin-inst-master-sql-business-partner-data.sql
+++ data-platform-fin-inst-master-sql-business-partner-data.sql
@@ -2,11 +2,11 @@ CREATE TABLE `data_platform_fin_inst_master_business_partner_data`  -- 新規追
 (
     `FinInstCountry`           varchar(3) NOT NULL,          -- 新規追加 
     `FinInstNumber`            varchar(15) NOT NULL,         -- 新規追加
-    `AccountBusinessPartner`   int(10) NOT NULL,             -- 新規追加
+    `FinInstBusinessPartner`   int(10) NOT NULL,             -- 新規追加
     `ValidityEndDate`          varchar(80) NOT NULL,         -- 新規追加
     `ValidityStartDate`        varchar(80) DEFAULT NULL,     -- 新規追加
     `IsMarkedForDeletion`      tinyint(1) DEFAULT NULL,      -- 新規追加
-    PRIMARY KEY (`FinInstCountry`, `FinInstNumber`, `AccountBusinessPartner`, `ValidityEndDate`),
+    PRIMARY KEY (`FinInstCountry`, `FinInstNumber`, `FinInstBusinessPartner`, `ValidityEndDate`),
     CONSTRAINT `DataPlatformFinInstMasterBusinessPartnerData_fk` FOREIGN KEY (`FinInstCountry`, `FinInstNumber`) REFERENCES `data_platform_fin_inst_master_general_data` (`FinInstCountry`, `FinInstNumber`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
